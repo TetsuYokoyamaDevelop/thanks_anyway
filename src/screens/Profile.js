7cec8479bc81630b34857c72f0977177ebb5c21b
TetsuYokoyamaDevelop <t.y.develop.1021@gmail.com>
2020-11-04 23:00:32 +0900

    hooksに書き換え

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-class Profile extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <p>Profile</p>
-                <br/>
-                <Link to="/">Homeへ</Link>
-            </div>
-        );
-    }
+const Profile = () => {
+
+  return (
+      <div className="container">
+          <p>Profile</p>
+          <br/>
+          <Link to="/">Homeへ</Link>
+      </div>
+  );
 }
 
 export default Profile;
