69df480f58b73eeede47fd1af09ce4b679f0961e
TetsuYokoyamaDevelop <t.y.develop.1021@gmail.com>
2020-11-05 19:45:48 +0900

    fix for deploy error

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import firebase from '../Firebase';
 import { makeStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CustomCard from '../components/CustomCard';
 
