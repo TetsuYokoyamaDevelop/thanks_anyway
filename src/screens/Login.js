76052b1810c0eefa6708bb0f92c7a2636ed08046
TetsuYokoyamaDevelop <t.y.develop.1021@gmail.com>
2020-11-05 16:39:34 +0900

    login more rich with material ui

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,33 +1,70 @@
 import React, { useContext } from "react";
 import { withRouter } from "react-router";
 import { AuthContext } from "../AuthProvider";
+// MUI Core
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Login = ({ history }) => {
+
   const { login } = useContext(AuthContext);
 
   // AuthContextからlogin関数を受け取る
   const handleSubmit = event => {
+    console.log('todoita??');
     event.preventDefault();
     const { email, password } = event.target.elements;
     login(email.value, password.value, history);
   };
 
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Log in</h1>
+    <Container className={classes.container} maxWidth="xs">
+      <h1>ようこそ！！！</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Email"
+                  name="email"
+                  size="small"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  size="small"
+                  type="password"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="secondary" fullWidth type="submit" variant="contained">
+              Log in
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Container>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(3),
+  },
+}));
 
 export default withRouter(Login);
